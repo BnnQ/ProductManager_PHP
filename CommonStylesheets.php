8164e087a6bc409d772218085eb6077110ff6164
BnnQ <1v25fa94@gmail.com>
2023-06-29 14:39:21 +0300

    Initial commit

---
+++ CommonStylesheets.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" href="/ProductManager/stylesheets/bootstrap.css"/>
+<link rel="stylesheet" href="/ProductManager/node_modules/@fortawesome/fontawesome-free/css/all.min.css"/>
+<link rel="stylesheet" href="/ProductManager/stylesheets/site.css"/>
+<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
